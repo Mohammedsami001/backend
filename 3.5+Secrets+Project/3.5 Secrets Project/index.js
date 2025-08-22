6e1bf78fb94d7d1e876a739b1339c8f4df2a0fee
Mohammedsami Sanadi <mohdsamisanadi@gmail.com>
2025-08-22 23:46:37 +0530

    secret project done

--- 3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ 3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -5,23 +5,37 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import e from "express";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;  
 
+var isAuthenticated = false;  
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkAuthentication(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    isAuthenticated = true;
+} 
+  next();
+}
+
+app.use(checkAuthentication);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");       
 });
 
-app.post("/check", bodyParser.urlencoded({ extended: true }), (req, res) => {
-  if (req.body.password === "ILoveProgramming") {
+
+app.post("/check", (req, res) => {
+  if (isAuthenticated) {
     res.sendFile(__dirname + "/public/secret.html");
   }
     else {
-      res.send("Wrong password! Try again."); 
+      res.sendFile(__dirname + "/public/index.html"); 
     }
   });
 

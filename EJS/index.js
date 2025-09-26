8ceb4b359924687905793bd0d55a06259808f07a
Mohammedsami Sanadi <mohdsamisanadi@gmail.com>
2025-09-26 13:01:45 +0530

    weekdayko weekend kiya

--- EJS/index.js
+++ EJS/index.js
@@ -1,27 +1,27 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const port = 3000;      
+const port = 3000;
 
 app.get("/", (req, res) => {
-    const today = new Date
-    const day = today.getDay();
+  const today = new Date();
+  const day = today.getDay();
 
-    if (day === 0 || day === 6) {
-        res.render("index.ejs", {
-            dayType: "weekend",
-            advice: "It's time to party!"
-        });       
-        return;
-        }
-    else {
-        res.render("index.ejs", {
-            dayType: "weekday",
-            advice: "It's time to work hard!"
-        }); 
-    }
-}); 
+  if (day === 0 || day === 6) {
+    res.render("index.ejs", {
+      dayType: "weekend",
+      advice: "It's time to party!",
+    });
+    return;
+  } else {
+    res.render("index.ejs", {
+      dayType: "weekend",
+      advice: "Time to work!",
+    });
+    return;
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-}); 
+});
